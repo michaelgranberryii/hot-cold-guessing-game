e6ba95c0ba9e4312fda0b39b8ee557df4e6657bc
michaelgranberryii <michaelgranberryii@gmail.com>
2023-02-04 19:12:40 -0800

    added code

--- src/rand_gen.vhd
+++ src/rand_gen.vhd
@@ -13,6 +13,20 @@ end rand_gen;
 
 architecture Behavioral of rand_gen is
 
+signal shift_reg : std_logic_vector(7 downto 0);
+signal feedback : std_logic; 
 begin
+
+process(clk, rst)
+begin
+if rst = '1' then
+    shift_reg <= seed;
+elsif rising_edge(clk) then
+    shift_reg <= shift_reg(6 downto 0) & feedback;
+end if;
+end process;
+
+feedback <= shift_reg(3) xor shift_reg(4) xor shift_reg(5);
+output <= shift_reg(7 downto 4);
 
 end Behavioral;
